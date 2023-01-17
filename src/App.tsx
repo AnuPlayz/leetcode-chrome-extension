4975177a395acd083aae9f233ee386f1cd182224
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-01-17 08:28:38 +0000

    a

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>I am typicall noone</h1>
+      <p style={{
+        textAlign: "right"
+      }}>- wrench</p>
+    </>
+  );
+}
+
+export default App;
